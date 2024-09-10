d342b225e7a84ea5448077d6daf83ffed570127b
indroji-mondal <indro.cse.bu@gmail.com>
2024-09-11 02:44:15 +0600

    added 4 problem

--- problem4.js
+++ problem4.js
@@ -1,3 +1,27 @@
 function calculateFinalScore(obj) {
-    //write your code here
+    if(typeof obj !=='object')
+    {
+        return 'Invalid Input';
+    }
+    const array1= Object.keys(obj);
+    const array2= Object.values(obj);
+
+    if(array1.includes('name')===false || array1.includes('testScore')===false  || array1.includes('schoolGrade')===false || array1.includes('isFFamily')===false )
+    {
+        return 'Invalid Input';
+    }
+    else if(typeof array2[0] !=='string' || typeof array2[1] !=='number' || array2[1]>50 || typeof array2[2] !=='number' || array2[2]>30  || typeof array2[3] !=='boolean')
+    {
+        return 'Invalid Input';
+    }
+
+    let total=0;
+    total+=obj.testScore+obj.schoolGrade+ (obj.isFFamily ? 20: 0) ;
+    return total>=80 ? true: false;
+    
 }
+const output = calculateFinalScore(
+    { name: "Rajib", testScore: 15,  schoolGrade: 25, isFFamily : true  }
+    
+    );
+console.log(output);
--- submission.js
+++ submission.js
@@ -47,3 +47,25 @@ function checkDigitsInName(name) {
     
 
 }
+function calculateFinalScore(obj) {
+    if(typeof obj !=='object')
+    {
+        return 'Invalid Input';
+    }
+    const array1= Object.keys(obj);
+    const array2= Object.values(obj);
+
+    if(array1.includes('name')===false || array1.includes('testScore')===false  || array1.includes('schoolGrade')===false || array1.includes('isFFamily')===false )
+    {
+        return 'Invalid Input';
+    }
+    else if(typeof array2[0] !=='string' || typeof array2[1] !=='number' || array2[1]>50 || typeof array2[2] !=='number' || array2[2]>30  || typeof array2[3] !=='boolean')
+    {
+        return 'Invalid Input';
+    }
+
+    let total=0;
+    total+=obj.testScore+obj.schoolGrade+ (obj.isFFamily ? 20: 0) ;
+    return total>=80 ? true: false;
+    
+}
