b6157cfc2bb28d50707e961766ae44ce870a0e02
indroji-mondal <indro.cse.bu@gmail.com>
2024-09-11 03:24:55 +0600

    added probl5 solution

--- problem5.js
+++ problem5.js
@@ -1,3 +1,21 @@
 function  waitingTime(waitingTimes  , serialNumber) {
-    // You have to write your code here
+   
+   
+    if (Array.isArray(waitingTimes)===false|| typeof serialNumber !=='number' || isNaN(serialNumber)===true )
+    {
+        return 'Invalid Input';
+    }
+    
+    const lengthOfWaitingTimes= waitingTimes.length;
+    const number= (serialNumber - lengthOfWaitingTimes)-1;
+    let sum=0;
+    for (const time of waitingTimes) {
+        sum+=time;
+    }
+
+    const result = Math.round(sum/lengthOfWaitingTimes)*number;
+    return result;
+
 }
+const output = waitingTime([ 3, 5, 7, 11, 6 ], 10);
+console.log(output);
--- submission.js
+++ submission.js
@@ -69,3 +69,22 @@ function calculateFinalScore(obj) {
     return total>=80 ? true: false;
     
 }
+function  waitingTime(waitingTimes  , serialNumber) {
+   
+   
+    if (Array.isArray(waitingTimes)===false|| typeof serialNumber !=='number' || isNaN(serialNumber)===true )
+    {
+        return 'Invalid Input';
+    }
+    
+    const lengthOfWaitingTimes= waitingTimes.length;
+    const number= (serialNumber - lengthOfWaitingTimes)-1;
+    let sum=0;
+    for (const time of waitingTimes) {
+        sum+=time;
+    }
+
+    const result = Math.round(sum/lengthOfWaitingTimes)*number;
+    return result;
+
+}
